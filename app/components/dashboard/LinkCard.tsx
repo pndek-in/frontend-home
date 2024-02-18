894b55365aa2c5b4f6231ea65d938a469d91d402
jal <jaluwibowoaji@gmail.com>
2024-02-18 14:41:04 +0700

    fix: debug dashboard link page

--- app/components/dashboard/LinkCard.tsx
+++ app/components/dashboard/LinkCard.tsx
@@ -99,12 +99,14 @@ export default function LinkCard({ data, isSuccess }: LinkCardProps) {
       )}
 
       <div className=" flex justify-between w-full mt-4 pt-4 border-t">
-        <EditLinkModal
-          data={data}
-          isSuccess={isSuccess}
-          setIsModalVisible={setIsModalVisible}
-          isModalVisible={isModalVisible}
-        />
+        {/* {isModalVisible && (
+          <EditLinkModal
+            data={data}
+            isSuccess={isSuccess}
+            setIsModalVisible={setIsModalVisible}
+            isModalVisible={isModalVisible}
+          />
+        )} */}
         <div className=" flex space-x-4">
           {data.status === 1 && (
             <>
