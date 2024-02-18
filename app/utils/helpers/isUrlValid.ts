ddc75dae193f48825a2a56ab434c7c41660a9dc8
jal <jaluwibowoaji@gmail.com>
2024-02-18 15:13:32 +0700

    fix: regex to allow dash

--- app/utils/helpers/isUrlValid.ts
+++ app/utils/helpers/isUrlValid.ts
@@ -1,6 +1,6 @@
 const isUrlValid = (url: string): boolean => {
   const pattern = new RegExp(
-    /^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?\/?$/
+    /^((ftp|http|https):\/\/)?(www\.)?(?!.*(ftp|http|https|www\.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#-]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?\/?$/
   )
 
   return pattern.test(url)
