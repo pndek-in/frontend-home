cd5547e88c9bc9e4c8d6e6f127f447826048fa0f
jal <jaluwibowoaji@gmail.com>
2024-04-20 15:05:24 +0700

    feat: static pages

--- app/routes/_static.about.tsx
+++ app/routes/_static.about.tsx
@@ -1,7 +1,69 @@
+import type { MetaFunction } from "@remix-run/node"
+import { useTranslation } from "react-i18next"
+
+import { Article } from "~/components/shared"
+
+type Content = {
+  type: "paragraph" | "list"
+  text: string
+  childs?: string[]
+}
+
+type Article = {
+  title: string
+  contents: Content[]
+}
+
+export const handle = {
+  i18n: ["meta", "about"]
+}
+
+export const meta: MetaFunction = () => {
+  const { t } = useTranslation("meta")
+
+  return [
+    { title: t("meta-about-title") },
+    { name: "description", content: t("meta-about-desc") }
+  ]
+}
+
 export default function About() {
+  const { t } = useTranslation("about")
+  const articles: Article[] = [
+    {
+      title: "",
+      contents: [
+        {
+          type: "paragraph",
+          text: t("about-pndek-in")
+        },
+        {
+          type: "paragraph",
+          text: t("stacks"),
+          childs: [
+            "Remix",
+            "Tailwind CSS",
+            "Telegram Bot API",
+            "Express",
+            "Google Safe Browsing API",
+            "PostgreSQL",
+            "Redis",
+          ]
+        },
+        {
+          type: "paragraph",
+          text: t("inquiry")
+        }
+      ]
+    }
+  ]
+
   return (
-    <div className=" min-h-screen flex items-center justify-center">
-      <h1 className=" text-2xl text-white">Page under construction</h1>
+    <div className=" min-h-screen flex items-center p-4 sm:p-32 flex-col">
+      <Article
+        title={"Tentang pndek.in"}
+        articles={articles}
+      />
     </div>
   )
 }
--- app/routes/_static.privacy-policy.tsx
+++ app/routes/_static.privacy-policy.tsx
@@ -1,7 +1,142 @@
+import type { MetaFunction } from "@remix-run/node"
+import { useTranslation } from "react-i18next"
+
+import { Article } from "~/components/shared"
+
+type Content = {
+  type: "paragraph" | "list"
+  text: string
+  childs?: string[]
+}
+
+type Article = {
+  title: string
+  contents: Content[]
+}
+
+export const handle = {
+  i18n: ["meta", "privacy"]
+}
+
+export const meta: MetaFunction = () => {
+  const { t } = useTranslation("meta")
+
+  return [
+    { title: t("meta-privacy-title") },
+    { name: "description", content: t("meta-privacy-desc") }
+  ]
+}
+
 export default function PrivacyPolicy() {
+  const { t } = useTranslation("privacy")
+  const latestUpdate = "04-07-2024"
+  const articles: Article[] = [
+    {
+      title: t("introduction-title"),
+      contents: [
+        {
+          type: "paragraph",
+          text: t("introduction-content-1")
+        }
+      ]
+    },
+    {
+      title: t("collected-info-title"),
+      contents: [
+        {
+          type: "list",
+          text: t("collected-info-content-1")
+        },
+        {
+          type: "list",
+          text: t("collected-info-content-2")
+        },
+        {
+          type: "list",
+          text: t("collected-info-content-3")
+        },
+        {
+          type: "list",
+          text: t("collected-info-content-4")
+        }
+      ]
+    },
+    {
+      title: t("use-of-info-title"),
+      contents: [
+        {
+          type: "paragraph",
+          text: t("use-of-info-content-1"),
+          childs: [
+            t("use-of-info-content-1-1"),
+            t("use-of-info-content-1-2"),
+            t("use-of-info-content-1-3"),
+            t("use-of-info-content-1-4"),
+            t("use-of-info-content-1-5")
+          ]
+        }
+      ]
+    },
+    {
+      title: t("info-disclosure-title"),
+      contents: [
+        {
+          type: "paragraph",
+          text: t("info-disclosure-content-1"),
+          childs: [
+            t("info-disclosure-content-1-1"),
+            t("info-disclosure-content-1-2"),
+            t("info-disclosure-content-1-3"),
+            t("info-disclosure-content-1-4")
+          ]
+        }
+      ]
+    },
+    {
+      title: t("data-storage-title"),
+      contents: [
+        {
+          type: "paragraph",
+          text: t("data-storage-content-1")
+        }
+      ]
+    },
+    {
+      title: t("data-security-title"),
+      contents: [
+        {
+          type: "paragraph",
+          text: t("data-security-content-1")
+        }
+      ]
+    },
+    {
+      title: t("privacy-changes-title"),
+      contents: [
+        {
+          type: "paragraph",
+          text: t("privacy-changes-content-1")
+        }
+      ]
+    },
+    {
+      title: t("contact-title"),
+      contents: [
+        {
+          type: "paragraph",
+          text: t("contact-content-1")
+        }
+      ]
+    }
+  ]
+
   return (
-    <div className=" min-h-screen flex items-center justify-center">
-      <h1 className=" text-2xl text-white">Page under construction</h1>
+    <div className=" min-h-screen flex items-center p-4 sm:p-32 flex-col">
+      <Article
+        title={"Kebijakan Privasi pndek.in"}
+        articles={articles}
+        latestUpdate={latestUpdate}
+      />
     </div>
   )
 }
