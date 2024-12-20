f3b01b4eb4decc7581b89f6232ad18c759214afe
Jalu Wibowo Aji <jaluwibowoaji@SekolahMus-MacBook-Pro-2.local>
2024-12-20 15:18:55 +0700

    fix: change url validation

--- app/utils/helpers/isUrlValid.ts
+++ app/utils/helpers/isUrlValid.ts
@@ -1,7 +1,7 @@
 const isUrlValid = (url: string): boolean => {
   const urlWithoutQuery = url.split("?")[0]
   const pattern = new RegExp(
-    /^((ftp|http|https):\/\/)?(www\.)?(?!.*(ftp|http|https|www\.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#-]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?\/?$/
+    /^(ftp|http|https):\/\/[^\s/$.?#].[^\s]*$|^[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}([/][^\s]*)?$/
   )
 
   return pattern.test(urlWithoutQuery)
